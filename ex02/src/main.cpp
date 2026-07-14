ed1082ba7a7b2503e769ce3b8b7f29632bfc3d95
r1 home <tealeafdrink@proton.me>
2026-07-14 15:00:15 +0200

    main.cpp : Added clock function to calculte execution time. Added deque test

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,6 +1,10 @@
 #include "PmergeMe.hpp"
 #include <algorithm>
+#include <bits/stdc++.h> //fixed(), setprecision()
+#include <ctime>
+#include <deque>
 #include <exception>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <utility> //std::pair, std::make_pair
@@ -38,18 +42,53 @@ int main(int argc, char **argv)
 
   try
   {
+    clock_t start, end;
+    start = clock();
     std::vector<std::pair<int, int> > arr;
     std::vector<std::pair<int, int> > sorted;
     std::vector<std::pair<int, int> > pend;
     std::vector<std::pair<int, int> > rest;
     arr.reserve(argc - 1);
     parseArgs(argc, argv, arr);
-    PmergeMe<std::vector<std::pair<int, int> > > p(arr);
-    p.downSort(p.arr_);
-    for (std::vector<std::pair<int, int> >::iterator it = p.arr_.begin(); it != p.arr_.end(); ++it)
+    PmergeMe<std::vector<std::pair<int, int> > > v(arr);
+    v.downSort(v.arr_);
+    for (std::vector<std::pair<int, int> >::iterator it = v.arr_.begin();
+         it != v.arr_.end(); ++it)
     {
-      std::cout << "p.arr_ : " << it->first << std::endl;
+      std::cout << "v.arr_ : " << it->first << std::endl;
     }
+    end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    std::cout << "Time taken by program is : " << std::fixed << time_taken
+              << std::setprecision(5);
+    std::cout << " sec " << std::endl;
+  }
+  catch (std::exception &e)
+  {
+    std::cout << e.what() << std::endl;
+    return (1);
+  }
+  try
+  {
+    clock_t start, end;
+    start = clock();
+    std::deque<std::pair<int, int> > arr;
+    std::deque<std::pair<int, int> > sorted;
+    std::deque<std::pair<int, int> > pend;
+    std::deque<std::pair<int, int> > rest;
+    parseArgs(argc, argv, arr);
+    PmergeMe<std::deque<std::pair<int, int> > > d(arr);
+    d.downSort(d.arr_);
+    for (std::deque<std::pair<int, int> >::iterator it = d.arr_.begin();
+         it != d.arr_.end(); ++it)
+    {
+      std::cout << "d.arr_ : " << it->first << std::endl;
+    }
+    end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    std::cout << "Time taken by program is : " << std::fixed << time_taken
+              << std::setprecision(5);
+    std::cout << " sec " << std::endl;
   }
   catch (std::exception &e)
   {
